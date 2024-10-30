70b5945f12044d2057f14df12b49370c438d2d35
Jagpreet Grewal <jaggy1111@gmail.com>
2024-10-30 00:01:37 -0700

    Heroku, Minor Changes Heroku: + Summing up the previous commits, got Heroku working + Resend and MongoDB work + Got front-end .env properly configured - Testing will need to be redone. Go back to module.exports = {route, ...function}, then do const var1 = ... then do var1.route for './app'
    Minor Changes:
    + Fixed duration of this project

--- client/src/Body.js
+++ client/src/Body.js
@@ -41,7 +41,7 @@ function Experience() {
       title: 'My Portfolio',
       descriptions: ['Developed an interactive MERN-stack portfolio website showcasing my skills and projects, including a custom GODOT mini-game offering 3 minutes of free gameplay to enhance visitor engagement.', 
         'Improved software reliability by identifying and resolving bugs, achieving 93% coverage on server- and client-side code with Jest unit tests'],
-      duration: 'November 2023 - April 2024',
+      duration: 'September 2024 - October 2024',
       link: 'https://github.com/JagpreetGrewal/new-portfolio',
       skills: ['ReactJS', 'Node.js', 'Express', 'MongoDB', 'Axios', 'Resend', 'Heroku', 'Netlify', 'CSS', 'GODOT', 'Github', 'REST APIs'],
     },
