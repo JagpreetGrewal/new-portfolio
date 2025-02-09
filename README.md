b952ed97ad9202c238910cca3050f5e98020e299
Jagpreet Grewal <jaggy1111@gmail.com>
2025-02-08 18:32:34 -0800

    Changing text

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 > MERN-stack portfolio website showcasing skills, projects, and a GODOT mini-game
 
 ## Running
-Run `npm run start` in the server folder. An `.env` file must be in that folder with the Resend Token defined in `RESEND_URI=...`. (Do the same for `EMAIL_URI=...` and `MONGODB_URI`)
+Run `npm run local_start` in the server folder. An `.env` file must be in that folder with the Resend Token defined in `RESEND_URI=...`. (Do the same for `EMAIL_URI=...` and `MONGODB_URI`)
 
 Run `npm run start` in the client folder.
 
@@ -13,16 +13,40 @@ Hi! Welcome to my portfolio website. This was made using a MERN tech stack.
 - *Frontend:* React, JavaScript, HTML, CSS
 - *Email Service:* Resend API
 - *Database:* MongoDB
-- *Deployment:* (if applicable) Vercel, Heroku, Netlify
-- *Authentication:* (if applicable) JWT, OAuth
+- *Deployment:* Heroku, Netlify
 - *Version Control:* Git, GitHub
-- *Other Libraries:* Axios (for API requests)
+- *Other Libraries:* Axios (for API requests), Jest (for testing)
+
+## Hosting on Heroku
+### MongoDB
+First, create a MongoDB collection. You do not need to set schema validation rules. The inserted documents will have the following fields:
+- `_id`: Object
+- - `email`: "interestedEmail@email.com"
+- `contacted`: false
+
+### Heroku
+The server folder must be pushed directly to Heroku with `git subtree push --prefix server heroku master`
+
+Go to the settings for your Heroku app and set the following config variables with their tokens:
+- `EMAIL_URI` = The email you signed up with for Resend (if using the free version)
+- `MONGODB_URI` = Your MongoDB connection string to your collection
+- `RESEND_URI` = Your Resend token
+
+It may be necessary to set `PROJECT_PATH` to `server`.
+
+## Hosting on Netlify
+Give Netlify access to your cloned repository. The client folder must be accessed only. Change the following in the Deploy settings:
+- Set the base directory to `client`
+- Set the build command to `npm run build`
+- Set the publish directory to `client/build`
+
+In the environment variables, set `REACT_APP_SERVER_URI` to the url given for your deployed Heroku app.
 
 ## Testing
 Create a `server/coverage/jestVars.js` file with your `process.env.RESEND_URI="..."`, etc. variables. Run `npm run test` in the client and server folders.
 
 ## Reading Commits
-From a certain point near the beginning of this project, each commit message starts with a concise title describing what changed since the previous commit. The description is broken up into different sections:
+Some commit messages starts with a concise title describing what changed since the previous commit(s). The description is broken up into different sections:
 - `+` represents positive changes
 - `-` represents bugs or mis-steps
 - `*` represents TODO or speculation
--- client/src/Body.js
+++ client/src/Body.js
@@ -8,29 +8,30 @@ function Experience() {
       title: 'B.A.Sc. Computer Engineering',
       institution: 'Simon Fraser University',
       link: 'https://www.sfu.ca/',
-      description: 'Graduated May 2024'
+      description: 'Graduated'
     },
   ];
 
   const experienceItems = [
     {
-      title: 'Manufacturing Test Engineer, Co-op',
+      title: 'Software Engineer, Co-op',
       institution: 'Semtech (Sierra Wireless)',
       duration: 'September 2023 - April 2024',
       link: 'https://www.sierrawireless.com/',
-      descriptions: ['Developed a comprehensive Python test script to reduce testing time from 20+ min to 4 min by automating frequency changes, visualizing signal loss measurements, providing actionable analytics, and optimizing script performance',
-        'Wrote a C++ automated data transfer and logging tool, packaging logs from lab machines into organized folders and emailing them to a designated address weekly with the Mailio library',
-        'Upgraded a C# time and success measurement tool for lab machines, redesigning the dashboard to include a stop clock and historical success rate graph, as well as removing deadlock bugs by refactoring and optimizing code',
-        'Calibrated, configured, and tested 100+ modules assigned through JIRA tickets, achieving a 50% faster completion rate compared to historical expectations by optimizing workflows',
+      descriptions: ['Designed Python scripts to streamline hardware testing by automating radio frequency changes, reducing testing time from 20+ minutes to 4 minutes',
+        'Refactored a C++ data logging tool to securely package and email logs, implementing batch emailing and sanity testing',
+        'Improved test setup and measurement tools in C#, implementing multi-threading, ensuring accurate data collection, and reducing debugging time',
+        'Performed data analytics with Visual Basic scripts, reducing analysis time by 30% and ensuring compliance of lab machines within tolerance levels',
       ],
     },
     {
-      title: 'Deployment Technician, Co-op',
+      title: 'Deployment Engineer, Co-op',
       institution: 'Microserve',
       duration: 'January 2021 - August 2021',
       link: 'https://www.microserve.ca/',
-      descriptions: ['Set up 500+ electronics for clients, achieving a 97% client satisfaction score (CSAT) by ensuring all devices were configured accurately and met specifications',
-        'Operated with a different 3 to 8 person team every week, enhancing teamwork and guiding new employees, and successfully completed 100% of projects on schedule',
+      descriptions: ['Reduced installation time by 20% by implementing Batch and Python scripts that automatically queried and saved hardware information to an online database',
+        'Set up over 500 electronics for clients, ensuring compliance with technical and software specifications and mastering diverse installation procedures to achieve high accuracy and reliability',
+        'Contributed to dynamic teams of 3-8 members, ensuring timely project completion and training new employees',
       ],
     },    
   ];
@@ -47,28 +48,30 @@ function Experience() {
     },
     {
       title: 'Pathloss Testing',
-      descriptions: ['Designed a Python application that used 100+ SCPI commands to calculate pathloss in fixtures across 2.4 GHz and 5 GHz frequencies, increasing test efficiency by 500+% with data analytics and visualization techniques', 
-        'Improved user experience and decreased start time by 50% by writing a YAML configuration file and Windows Batch Script file'],
+      descriptions: ['Tested pathloss rapidly across 2.4 GHz and 5 GHz frequencies in lab machines by developing a Python application that integrated SCPI commands to communicate with the hardware', 
+        'Enchanced data accuracy by 70% by analyzing sensor data with NumPy', 
+        'Printed CSV and YAML logs that asserted compliance within pathloss limits and within a designated delta of historical logs by using Python scripts',],
       duration: 'November 2023 - April 2024',
       link: '',
-      skills: ['Python', 'Github', 'YAML', 'Batch Script', 'SCPI', 'RsCmwGprfMeas'],
+      skills: ['Python', 'Github', 'SCPI', 'RsCmwGprfMeas','YAML', 'Batch Script'],
     },
     {
       title: 'KickPro',
-      descriptions: ['Developed a dynamic ball-launcher on a Raspberry Pi, applying real-time image processing algorithms to track human targets and adjust launch speed using OpenCV and I2C',
-        'Designed and iterated on the prototype frame using AutoCAD and created a custom integrated circuit board (IC), using Git for version control and documenting the process with a Required Specifications Document.',
-        'Gave users data analytics and insights, providing shooting accuracy and ball placement in visual format (shot data over image of a net) with a Tkinter GUI'
+      descriptions: ['Designed and prototyped a ball-launcher system utilizing a Raspberry Pi, integrating I2C for hardware communication and OpenCV for real-time image processing',
+        'Coded wireless process sharing to improve OpenCV processing time by 100%, using Python and Bash scripts to offload work from the embedded device to a laptop',
+        'Created PCB designs using Altium and frame designs with AutoCAD, reducing development cycles by 25%'
       ],
       duration: 'January 2023 - August 2024',
-      link: 'https://github.com/minhphat97/Capstone/tree/Henry',
+      link: 'https://github.com/JagpreetGrewal/Capstone',
       skills: ['Python', 'Github', 'OpenCV', 'Bash Script', 'I2C', 'Tkinter'],
     },
     {
       title: 'Yet Another Storage Engine',
       descriptions: ['Implemented a relational database engine using optimized concurrent C++ code for indexing, locking, logging, and storage modules, enabling the engine to process over 300 transactions per second',
-        'Achieved synchronization of data-intensive transactions with a 0% rate of deadlocking, implementing stress testing and monitoring alongside threads, processes, and hierarchical locking rules with a wait-die policy'
+        'Achieved synchronization of data-intensive transactions with a 0% rate of deadlocking, implementing stress testing and monitoring alongside threads, processes, and hierarchical locking rules with a wait-die policy',
+        'Improved database reliability by identifying and resolving bugs through implementation of unit tests with GoogleTest',
       ],
-      duration: 'September 2022 - December 2022',
+      duration: 'January 2023 - August 2023',
       link: '#',
       skills: ['C++', 'CMake', 'Gitlab'],
     },
