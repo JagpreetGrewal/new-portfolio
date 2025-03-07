8604675c6b241226d8ac68186eebce3e213626cc
Jagpreet Grewal <jaggy1111@gmail.com>
2025-03-06 16:15:48 -0800

    removing unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 import Headers from './Header';
 import MyBody from './Body';
